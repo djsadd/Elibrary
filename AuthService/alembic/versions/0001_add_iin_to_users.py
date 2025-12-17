642684fc5ac818d845d783a5aec427cd28ec5ffe
Erasil <erasil.bakhytgan@gmail.com>
2025-12-17 12:53:42 +0500

    add iin to register

---
+++ AuthService/alembic/versions/0001_add_iin_to_users.py
@@ -0,0 +1,25 @@
+"""add iin to users
+
+Revision ID: 0002_add_iin_to_users
+Revises:
+Create Date: 2025-12-17
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "0002_add_iin_to_users"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Use IF NOT EXISTS to be safe if column already added manually.
+    op.execute(
+        "ALTER TABLE users ADD COLUMN IF NOT EXISTS iin VARCHAR(12)"
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "iin")
