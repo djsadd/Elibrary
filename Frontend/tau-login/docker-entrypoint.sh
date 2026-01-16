7d83b0ae2f7ea19eb149df80ed5391447334a547
Yerassyl <e.bahytzhanuly@tau-edu.kz>
2026-01-16 14:28:10 +0500

    add search service

--- Frontend/tau-login/docker-entrypoint.sh
+++ Frontend/tau-login/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -euo pipefail
 : "${API_SERVER:=http://backend:8000}"
-envsubst '${API_SERVER}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+: "${CLIENT_MAX_BODY_SIZE:=200m}"
+envsubst '${API_SERVER} ${CLIENT_MAX_BODY_SIZE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec "$@"
